d39f34ceeec9340006adfffffea965c088f8c0dc
Bekmurodo <zsuhrob240@gmail.com>
2025-01-25 18:31:04 +0300

    All code about drf authentication

---
+++ users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from.models import User, UserConfirmation
+
+
+#class AdminModel(admin.ModelAdmin):
+#    list_display = ["id", "phone_number"]
+
+#admin.site.register(User, AdminModel)
+#admin.site.register(UserConfirmation)
